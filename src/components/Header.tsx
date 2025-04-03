3c908c81ae817d151fb3cae27bf2bafacaa0a80c
Santiago Boselli <sboselli@gmail.com>
2025-04-03 11:29:16 -0300

    Remove buy coffe from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -78,7 +78,7 @@ export function Header({ onSearch }: HeaderProps) {
             </button>
 
             {/* Buy Me a Coffee Button */}
-            <a
+            {/* <a
               href="https://www.buymeacoffee.com/damburudhar"
               target="_blank"
               rel="noopener noreferrer"
@@ -95,7 +95,7 @@ export function Header({ onSearch }: HeaderProps) {
                 <path d="M20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2c1.11 0 2-.89 2-2V5c0-1.11-.89-2-2-2zm0 5h-2V5h2v3zM4 19h16v2H4z"/>
               </svg>
               Buy me a coffee
-            </a>
+            </a> */}
 
             {/* Submit Button */}
             <Link
